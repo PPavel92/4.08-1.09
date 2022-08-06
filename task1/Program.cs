43b0b4851ae87906999ef3c3cf2a9caef662144c
PavelProskuryakovp <PPavel@mail.ru>
2022-08-06 10:22:49 +0600

    new repository

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// 1. Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+
+Console.Write("Введите первое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int square = number2 * number2;
+if (number == square)
+{
+    Console.WriteLine($"Число {number} является квадратом {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {number} не является квадратом {number2}");
+}
---
+++ task3/Program.cs
@@ -0,0 +1,41 @@
+﻿// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.Write("Введите число от 1 до 7:_");
+int number = Convert.ToInt32(Console.ReadLine());
+string day = "";
+if (number <= 7)
+{
+    switch (number)
+    {
+        case 1:
+            day = "Понедельник";
+            break;
+        case 2:
+            day = "Вторник";
+            break;
+        case 3:
+            day = "Среда";
+            break;
+        case 4:
+            day = "Четверг";
+            break;
+        case 5:
+            day = "Пятница";
+            break;
+        case 6:
+            day = "Суббота";
+            break;
+        case 7:
+            day = "Воскресенье";
+            break;
+
+    }
+    Console.WriteLine("День недели:" + day);
+}
+else
+{
+    Console.WriteLine("Вы ввели некоректное число");
+}
