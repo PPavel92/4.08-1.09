3ef9e394b424c285aa2e0457ae44fb4df72de0bc
PavelProskuryakovp <PPavel@mail.ru>
2022-08-06 14:55:13 +0600

    add home work

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает 
+// два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число:");
+int numbers0 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int numbers1 = Convert.ToInt32(Console.ReadLine());
+int max = numbers0;
+int min = numbers1;
+if (max < min)
+{
+    max = numbers1;
+    min = numbers0;
+} 
+Console.WriteLine($"Число {max} больше числа {min}");
---
+++ task4/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 4: Напишите программу, которая принимает 
+// на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите 1 число:");
+int number0 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3 число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number0;
+int max1 = number1;
+if (max1 > number2)
+{
+ max1 = number1;
+}
+else 
+{
+    max1 = number2;
+}
+if (max>max1)
+{
+    max = number0;
+}
+else
+{
+    max = max1;
+}
+Console.Write($"Максимальное число из чисел {number0}; {number1}; {number2} будет число {max}");
