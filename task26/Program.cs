cfd30d5826d2140b6e803d7e40f727123b97e093
PavelProskuryakovp <PPavel@mail.ru>
2022-08-29 21:27:03 +0600

    work4

---
+++ task26/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+Console.WriteLine("Введите целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int CountNumbers(int num)
+{
+int counter =1;
+while (number >= 10)
+{
+    number = number /10;
+    counter++;
+}
+return counter;
+}
+int result = CountNumbers(number);
+Console.WriteLine($"Колличество цифр в числе {number} = {result}");
+
+// for (; number >=10; number = number /10)
+// {
+//     counter++;
+// }
+// Console.WriteLine($"Колличество цифр в числе  = {counter}");
---
+++ task28/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число :");
+int num = Convert.ToInt32(Console.ReadLine());
+int MultNum(int n)
+{
+    int m = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        m *= i;
+    }
+    return m;
+}
+int result = MultNum(num);
+Console.WriteLine($"Произведение чисел от 1 до {num} -> {result}");
