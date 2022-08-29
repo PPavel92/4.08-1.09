f50ac8c4e0b755b56bfb77a93e78e2175e6b7b8a
PavelProskuryakovp <PPavel@mail.ru>
2022-08-29 18:09:23 +0600

    home work

---
+++ task11/Program.cs
@@ -0,0 +1,19 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random(). Next(100, 1000);
+Console.WriteLine($"Сгенерированное число {number}");
+
+ int DeltwoDigit (int num)
+{
+ int firstDigit = number /100;
+ int secondDigit = number % 10;
+ int result = firstDigit *10 + secondDigit;
+ return result;
+}
+ int res = DeltwoDigit(number);
+ Console.WriteLine(res);
--- task9/Program.cs
+++ task9/Program.cs
@@ -4,3 +4,34 @@
 // 78 -> 8
 // 12-> 2
 // 85 -> 8
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($" Случайное число из отрезка 10 - 99 => {number}");
+
+// int firstDigit = number / 10;
+// int secondDigit = number %10;
+
+// if (firstDigit == secondDigit) Console.WriteLine("Числа равны");
+// else
+// {
+// // int max = firstDigit;
+// // if (secondDigit > firstDigit) max = secondDigit;
+// int max = firstDigit > secondDigit ? firstDigit : secondDigit;
+
+// Console.WriteLine($"Наибольшая цифра числа {number} =>");
+// Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit);
+
+// // Console.WriteLine($"Наибольшая цифра числа {number} => {max}");
+// }
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    if(firstDigit == secondDigit) return -1;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+int maxDigit = MaxDigit(number);
+string result = maxDigit != -1 ? maxDigit.ToString() : "Цифры одинаковые";
+Console.WriteLine($"Наибольшая цифра числа {number} => {maxDigit}");
