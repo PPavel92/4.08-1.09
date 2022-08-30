957c110970ddddb4fa3d6a06c22549617ee7b6ad
PavelProskuryakovp <PPavel@mail.ru>
2022-08-30 12:19:57 +0600

    home  work4

---
+++ task25/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 25: Напишите цикл,
+//  который принимает на вход два числа (A и B) 
+//  и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+
+Console.Write("Введите число А:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B:");
+int b = Convert.ToInt32(Console.ReadLine());
+int NaturaDegree(int an, int bn)
+{
+    int am = 1;
+    for (int i = 1; i <= bn; i++) am = am * an; 
+    return am;
+}
+int result = NaturaDegree(a, b);
+Console.WriteLine($"Число A {a} степени В {b} -> {result}");
+
+
+
+
+
---
+++ task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, 
+// которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число :");
+int num = Convert.ToInt32(Console.ReadLine());
+int SumNum(int n)
+{
+    int count = Convert.ToString(n).Length;
+    int str = 0;
+    int res = 0;
+ for (int i = 0; i < count; i++)
+    {
+        str = n - n % 10;
+        res = res + (n - str);
+        n = n / 10;
+    }
+    return res;
+}
+int sum = SumNum(num);
+Console.Write($"Cумму цифр в числе {num} -> {sum} {str}");
