f4b6f0d8de39cfcfe5ad556c71dd07ed2b751901
Gary <Gary@gmail.com>
2022-09-08 05:38:25 +0600

    homework7

--- task50/Program.cs
+++ task50/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 50. Напишите программу, которая на вход принемает позиции элемента в двухмерном массиве,
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двухмерном массиве,
 //  и возвращает значение этого элемента или же указание, что такого элемента нет.
 //  Например, задан массив:
 //  1 4 7 2
@@ -10,9 +10,9 @@ int[,] CreateMatrixRndInt(int row, int col, int min, int max)
 {
     int[,] matrix = new int[row, col];
     Random rnd = new Random();
-    for (int i = 0; i < matrix.GetLength(0); i++)       
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)  
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
             matrix[i, j] = rnd.Next(min, max + 1);
         }
@@ -36,12 +36,12 @@ void PrintMatrix(int[,] matrix)
 
 Console.WriteLine("Введите первую позицию");
 int position1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите вторую позицию ");
+Console.WriteLine("Введите вторую позицию ");                        /// Недоделал!!!
 int position2 = Convert.ToInt32(Console.ReadLine());
 
 int PositionElement(int[,] matrix, int a )
 {
-    
+
  for (int i = 0; i <position1 ; i++)
  {
     a = i;
@@ -49,7 +49,8 @@ int PositionElement(int[,] matrix, int a )
  }
  return a;
 }
-
- int[,] matr = CreateMatrixRndInt(3, 4, 1, 9);
+int[,] matr = CreateMatrixRndInt(3, 4, 1, 9);
 PrintMatrix(matr);
+
 int result = PositionElement(matr);
+
