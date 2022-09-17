845571d545a6f9ae3221caae8a050165fd79719f
PavelProskuryakovp <PPavel@mail.ru>
2022-09-17 15:11:52 +0600

    HomeWork9

---
+++ task66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N.
+// Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите целое положительное число A");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число B");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int SumBetween(int am, int an)
+{
+  if (am > an) return am;
+ if (an==am) return am;
+ return am + SumBetween(am+1, an);
+}
+int result = SumBetween(numberM, numberN);
+Console.WriteLine(result);
---
+++ task68/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+// m = 3 n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите первой целое положительное число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое положительное число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+int MetodAkkerman(int numM, int numN)
+{
+  if (numM == 0)
+    return numN + 1;
+  else
+    if ((numM != 0) && (numN == 0))
+      return MetodAkkerman(numM - 1, 1);
+    else
+      return MetodAkkerman(numM - 1, MetodAkkerman(numM, numN - 1));
+}
+
+int result = MetodAkkerman(num1, num2);
+Console.WriteLine(result);
